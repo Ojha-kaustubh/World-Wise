2f03982ee46cf4d32b9ef6dca9f30cd73020491a
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-14 18:45:24 +0530

    create the ui of this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "./pages/product";
+import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout"
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="product" element={<Product />} />
           <Route path="Pricing" element={<Pricing />} />
           <Route path="app" element={<AppLayout />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />}/>
         </Routes>
       </BrowserRouter>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,8 @@
 
 import React from 'react'
-// import App from '../App'
-import AppNav from '../components/AppNav'
 
 export default function AppLayout() {
   return (
-    <div>
-        <AppNav />
-    </div>
+    <div>AppLayout</div>
   )
 }
